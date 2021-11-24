b96b65135897ce6cc6f2c49202070a9156d6348c
Dmitri233 <36127740+Dmitri233@users.noreply.github.com>
2021-11-24 21:10:16 +0800

    不使用麻烦的双引号+错字修复

--- docs/.vuepress/config.js
+++ docs/.vuepress/config.js
@@ -1,8 +1,8 @@
 module.exports = {
-  title: "SCCCP Community Server Wiki",
-  description: "SCCCPServerWikiMark",
+  title: 'SCCCP Community Server Wiki',
+  description: 'SCCCPServerWikiMark',
   themeConfig: {
-    logo: "./assets/img/server-icon.png",
+    logo: './assets/img/server-icon.png',
     lastUpdated: '最后更新',
     locales: {
       '/': {
@@ -10,18 +10,18 @@ module.exports = {
       }
     },
     nav: [
-      { text: "主页", link: "/" },
-      { text: "服务器介绍", link: "/preface" },
-      { text: "目录", link: "/dirs" },
-      { text: "论坛社区", link: "https://bbs.playl.net/forums/scccp/" }
+      { text: '主页', link: '/' },
+      { text: '服务器介绍', link: '/preface' },
+      { text: '目录', link: '/dirs' },
+      { text: '论坛社区', link: 'https://bbs.playl.net/forums/scccp/' }
     ],
     sidebar: [
-      ["/", "主页"],
-      ["/dirs", "目录"],
+      ['/', '主页'],
+      ['/dirs', '目录'],
     ],
-    repo: "SCCCP-Team/SCCCPServerWiki",
+    repo: 'SCCCP-Team/SCCCPServerWiki',
     editLinks: true,
-    editLinkText: "在Github上编辑",
+    editLinkText: '在Github上编辑',
   },
   plugins: {
     '@vuepress-reco/bgm-player': {

d889eb7aef9c2107aee9efe8a0b6a281edc14a9c
Dmitri <36127740+Dmitri233@users.noreply.github.com>
2021-11-28 17:51:17 +0800

    完善readme.md 稍微完善command页面

--- README.md
+++ README.md
@@ -1,1 +1,16 @@
 这里是SCCCP社区服务器的官方WIKI, 欢迎贡献(PR请求), 被接受的贡献可获得服务器内5000奖励可占领方块数
+
+## 如何在本机构建测试环境
+在开始前,需要自行安装nodejs  
+安装yarn  
+```sh
+npm install --global yarn  
+```
+配置项目依赖  
+```sh
+yarn install  
+```
+运行开发服务器  
+```sh
+yarn run docs:dev  
+```
--- docs/.vuepress/config.js
+++ docs/.vuepress/config.js
@@ -1,45 +1,58 @@
 module.exports = {
-  title: 'SCCCP Community Server Wiki',
-  description: 'SCCCPServerWikiMark',
+  title: "SCCCP Community Server Wiki",
+  description: "SCCCPServerWikiMark",
   themeConfig: {
-    logo: './assets/img/server-icon.png',
-    lastUpdated: '最后更新',
+    logo: "./assets/img/server-icon.png",
+    lastUpdated: "最后更新",
     nav: [
-      { text: '主页', link: '/' },
-      { text: '服务器介绍', link: '/preface' },
-      { text: '目录', link: '/dirs' },
-      { text: '论坛社区', link: 'https://bbs.playl.net/forums/scccp/' }
+      { text: "主页", link: "/" },
+      { text: "服务器介绍", link: "/preface" },
+      { text: "目录", link: "/dirs" },
+      { text: "论坛社区", link: "https://bbs.playl.net/forums/scccp/" },
     ],
     sidebar: [
-      ['/', '主页'],
+      ["/", "主页"],
+      ["/guide", "新手快速入门"],
       {
-        title: '目录',
-        path: '/dirs',
+        title: "目录",
         collapsable: false,
         sidebarDepth: 1,
         children: [
-          ['/command/tpa', 'TPA命令'],
+          ["/preface", "服务器介绍"],
+          {
+            title: "命令",
+            collapsable: true,
+            sidebarDepth: 1,
+            children: [
+              ["/command/tpa", "Tpa(玩家间传送)"],
+              ["/command/tpoint", "Tpoint(传送点功能)"],
+              ["/command/diffmod", "df(难度调整功能)"],
+              ["/command/landclaim", "landclaim(私人领地标记)"],
+              ["/command/coreprotect", "c(破坏/容器交互查询)"],
+              ["/command/other", "杂项命令"]
+            ],
+          }
         ]
-      },
+      }
     ],
-    
-    repo: 'SCCCP-Team/SCCCPServerWiki',
-    docsDir: 'docs',
+
+    repo: "SCCCP-Team/SCCCPServerWiki",
+    docsDir: "docs",
     editLinks: true,
-    editLinkText: '在Github上编辑',
+    editLinkText: "在Github上编辑",
   },
   plugins: {
-    '@vuepress-reco/bgm-player': {
+    "@vuepress-reco/bgm-player": {
       audios: [
         {
-          name: 'Otherside',
-          artist: 'Lena Raine',
-          url: '/assets/bgm/Otherside.ogg',
-          cover: '/assets/bgm/Otherside.png'
-        }
+          name: "Otherside",
+          artist: "Lena Raine",
+          url: "/assets/bgm/Otherside.ogg",
+          cover: "/assets/bgm/Otherside.png",
+        },
       ],
       autoShrink: true,
-      shrinkMode: 'float',
-     }
-  }
+      shrinkMode: "float",
+    },
+  },
 };

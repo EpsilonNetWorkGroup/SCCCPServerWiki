6cabfcf58ed00bc086b90cab6d287a28114c05a7
CO-UnknowKE <none>
2024-03-10 17:25:43 +0800

    update playlist file link

--- docs/.vitepress/theme/index.ts
+++ docs/.vitepress/theme/index.ts
@@ -8,23 +8,23 @@ const playlist = [
   {
     name: 'Relic',
     author: 'Aaron Cherof',
-    file: 'https://gitcode.net/Dmitri233/SCCCPServerWikiPublicFile/-/raw/main/bgm/Relic.ogg',
+    file: '/bgm/Relic.ogg',
   },
   {
     name: '5',
     author: 'Samuel Åberg & Jagdaggar Remix',
-    file: 'https://gitcode.net/Dmitri233/SCCCPServerWikiPublicFile/-/raw/main/bgm/Disc%205%20Jagdaggar%20Remix.mp3',
+    file: '/bgm/Disc%205%20Jagdaggar%20Remix.mp3',
   },
   {
     name: 'Infinite Amethyst',
     author: 'Lena Raine',
-    file: 'https://gitcode.net/Dmitri233/SCCCPServerWikiPublicFile/-/raw/main/bgm/Infinite%20Amethyst.ogg',
+    file: '/bgm/Infinite%20Amethyst.ogg',
     hide: false
   },
   {
     name: 'Otherside',
     author: 'Lena Raine',
-    file: 'https://gitcode.net/Dmitri233/SCCCPServerWikiPublicFile/-/raw/main/bgm/Otherside.ogg',
+    file: '/bgm/Otherside.ogg',
     hide: false
   },
 ]
